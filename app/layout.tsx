8471a985933bdc06a2da1ac3b833fbe4dc103a67
Godwin Gabriel <godwinpeace22@gmail.com>
2025-09-11 14:47:16 +0100

    fix header

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Home | CrossAtlanticProperties",
+  description: "",
 };
 
 export default function RootLayout({
